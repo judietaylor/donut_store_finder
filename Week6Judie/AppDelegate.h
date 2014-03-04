b68525ce97d10357875deca03566db1af19a6759
Judie Taylor <judietaylor@Judies-MacBook-Pro.local>
2014-03-04 18:00:22 -0500

    Initial Commit

---
+++ Week6Judie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week6Judie
+//
+//  Created by Judie Taylor on 3/4/14.
+//  Copyright (c) 2014 Judie Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week6JudieTests/Week6JudieTests.m
@@ -0,0 +1,34 @@
+//
+//  Week6JudieTests.m
+//  Week6JudieTests
+//
+//  Created by Judie Taylor on 3/4/14.
+//  Copyright (c) 2014 Judie Taylor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week6JudieTests : XCTestCase
+
+@end
+
+@implementation Week6JudieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
